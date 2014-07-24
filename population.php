fc92f310cffd7f4aa8d42097c6b351de888ed683
Dannie <Dan@caviart.co>
2014-07-24 21:15:03 +0000

    updated the population php

--- population.php
+++ population.php
@@ -3,13 +3,27 @@
 // Creating the variables to plug into the mysql_connect() function
 // is optional, as they can be entered “manually.”
 $connection = mysql_connect('127.0.0.1', 'root', 'basketbal1');
-mysql_select_db('day-4-exercises');
+mysql_select_db('day4exercises');
 // mysql_connect(‘SQLdata.com’, ‘Jones’, ‘un1c0rn’, ‘my_database’);
+
 
 // Check connection
 if (!$connection) {
   die('Unable to connect: ' . mysql_errno());
 }
 
+if ($connection) {
+  //die('YAY: ' . mysql_errno());
+  // SQL Query
+  $result = mysql_query('SELECT city_name, population FROM population');
+
+  // Fetch the data from the result
+  while ($row = mysql_fetch_array($result)) {
+    echo "City Name: " . $row['city_name'] . "<br>" .
+         "Population: "  . $row['population']  . "<br>";
+  }
+}
+
+mysql_close($connection);
 ?>
 
